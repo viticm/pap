c36f654eeaa64801628c7588e5122263ef7ce955
viticm <viticm@126.com>
2013-12-03 11:34:13 +0800

    base structure for share memroy model

---
+++ utf8/include/common/sys/share_memory.h
@@ -0,0 +1,32 @@
+/**
+ * PAP Engine ( https://github.com/viticm/pap )
+ * $Id thread.h
+ * @link https://github.com/viticm/pap for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-12-3 11:32:43
+ * @uses system share memory model
+ */
+#ifndef PAP_COMMON_SYS_SHARE_MEMORY_H_
+#define PAP_COMMON_SYS_SHARE_MEMORY_H_
+
+namespace pap_common_sys {
+
+namespace share_memory {
+
+namespace api {
+
+}; //namespace api
+
+class Base {
+};
+
+class Manager {
+};
+
+}; //namespace share_memory
+
+}; //namespace pap_common_sys
+
+#endif //PAP_COMMON_SYS_SHARE_MEMORY_H_
--- utf8/include/common/sys/thread.h
+++ utf8/include/common/sys/thread.h
@@ -1,3 +1,13 @@
+/**
+ * PAP Engine ( https://github.com/viticm/pap )
+ * $Id thread.h
+ * @link https://github.com/viticm/pap for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-11-23 10:27:23
+ * @uses system about thread classes
+ */
 #ifndef PAP_COMMON_THREAD_H_
 #define PAP_COMMON_THREAD_H_
 
