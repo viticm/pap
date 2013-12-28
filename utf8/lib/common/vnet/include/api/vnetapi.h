df322b501b4627a543c39db6430ae8060e40ad66
viticm <viticm@126.com>
2013-12-28 10:58:13 +0800

    optimize lib vnet

---
+++ utf8/lib/common/vnet/include/api/vnetapi.h
@@ -0,0 +1,17 @@
+/**
+ * PAP Engine ( https://github.com/viticm/pap )
+ * model vnet
+ * $Id vnetapi.h
+ * @link https://github.com/viticm/pap for the canonical source repository
+ * @copyright Copyright (c) 2013-2013 viticm( viticm@126.com )
+ * @license
+ * @user viticm<viticm@126.com>
+ * @date 2013-12-27 17:15:18
+ * @uses the vnet all apis
+ */
+#ifndef VNET_API_VNETAPI_H_
+#define VNET_API_VNETAPI_H_
+
+#include "api/vnetconf.h"
+
+#endif //VNET_API_VNETAPI_H_
