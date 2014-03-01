717809e6f37d3dc671c1d7a48b696f72247f5874
viticm <viticm@126.com>
2014-03-01 12:39:37 +0800

    change the tests code style

--- utf8/lib/common/vnet/tests/src/system/client.c
+++ utf8/lib/common/vnet/tests/src/system/client.c
@@ -8,43 +8,42 @@
 
 #define MAXLINE 4096
 
-int main(int argc, char** argv)
-{
-    int    sockfd, n;
-    char    recvline[4096], sendline[4096];
-    struct sockaddr_in    servaddr;
+int main(int argc, char** argv) {
+  int sockfd, n;
+  char recvline[4096], sendline[4096];
+  struct sockaddr_in servaddr;
 
-    if( argc != 2){
+  if (argc != 2) {
     printf("usage: ./client <ipaddress>\n");
     exit(0);
-    }
+  }
 
-    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
     exit(0);
-    }
-    printf("sockfd: %d\n", sockfd);
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(6666);
-    if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
-    printf("inet_pton error for %s\n",argv[1]);
+  }
+    
+  printf("sockfd: %d\n", sockfd);
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(6666);
+  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    printf("inet_pton error for %s\n", argv[1]);
     exit(0);
-    }
+  }
 
-    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
-    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+  if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+    printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
     exit(0);
-    }
+  }
 
-    printf("send msg to server: \n");
-    fgets(sendline, 4096, stdin);
-    if( send(sockfd, sendline, strlen(sendline), 0) < 0)
-    {
+  printf("send msg to server: \n");
+  fgets(sendline, 4096, stdin);
+  if (send(sockfd, sendline, strlen(sendline), 0) < 0) {
     printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
     exit(0);
-    }
+  }
 
-    close(sockfd);
-    exit(0);
+  close(sockfd);
+  exit(0);
 }
