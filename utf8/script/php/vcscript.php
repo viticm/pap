26b0cdb0a1244b93fb905b9a748a59593dd31f63
viticm <viticm@126.com>
2014-03-16 11:10:17 +0800

    update vengine vc script

--- utf8/script/php/vcscript.php
+++ utf8/script/php/vcscript.php
@@ -8,6 +8,8 @@
  * ..... project must be have two part: src and include.
  */
 
+define('DELIMITER', '\\');
+define('LF', "\r\n");
 /**
  * get vc file from filename for vc script need include
  * @param int $pathdeep
@@ -18,19 +20,19 @@ function get_vcfile($pathdeep = 0, $isdir = false, $filename = NULL) {
   if (NULL === $filename) return NULL;
   $deeptab = "\t\t\t";
   if (false == $isdir) $deeptab .= "\t";
-  for ($i = 0; $i < $pathdeep; ++$i) {
+  for ($i = 0; $i < $pathdeep - 1; ++$i) {
     $deeptab .= "\t";
   }
   if (true === $isdir) {
     //$vcfile
-    $vcfile = $deeptab."<Filter";
-		$vcfile	.= $deeptab."\tName=\".$filename.\"";
-		$vcfile	.= $deeptab."\t>";
+    $vcfile = $deeptab."<Filter".LF;
+		$vcfile	.= $deeptab."\tName=\"{$filename}\"".LF;
+		$vcfile	.= $deeptab."\t>".LF;
   } else {
-    $vcfile = $deeptab."<File";
-    $vcfile .= $deeptab."\tRelativePath=\".$filename.\"";
-    $vcfile .= $deeptab."\t>";
-    $vcfile .= $deeptab."</File>";
+    $vcfile = $deeptab."<File".LF;
+    $vcfile .= $deeptab."\tRelativePath=\"{$filename}\"".LF;
+    $vcfile .= $deeptab."\t>".LF;
+    $vcfile .= $deeptab."</File>".LF;
   }
   return $vcfile;
 }
@@ -42,7 +44,7 @@ function get_vcfile($pathdeep = 0, $isdir = false, $filename = NULL) {
  * @return bool
  */
 function indir($dir, $needle) {
-  $pos = strpos($needle, '\\');
+  $pos = strpos($needle, DELIMITER);
   $fatherdir = substr($needle, 0, $pos);
   return $dir === $fatherdir;
 }
@@ -64,22 +66,55 @@ function complementpath($path, $delimiter = '/') {
 /**
  * find all files from the directory
  * @param string $path
+ * @param array $files
  * @return array
  */
-function traverse($path = '.') {
+function traverse($path = '.', &$files = array()) {
   $current_dir = opendir($path);
-  $files = array();
   while(($file = readdir($current_dir)) !== false) {
-    $sub_dir = $path . DIRECTORY_SEPARATOR . $file;
+    $sub_dir = $path.DIRECTORY_SEPARATOR.$file;
     if($file == '.' || $file == '..') {
       continue;
     } else if(is_dir($sub_dir)) {
-      traverse($sub_dir);
+      traverse($sub_dir, $files);
     } else {
       array_push($files, $sub_dir);
     }
   }
   return $files;
+}
+
+/**
+ * get directory path form directory array
+ * @param array $directorys
+ * @param int $key
+ * @return string
+ */
+function getdirectory($directorys = array(), $key = -1) {
+  $delimiter = '\\';
+  $result = '';
+  if (empty($directorys) || -1 === $key) return $result;
+  foreach ($directorys as $deep => $directory) {
+    $result .= $directory;
+    $result .= $delimiter;
+    if ($key == $deep) return $result;
+  }
+}
+
+/**
+ * get filter end info
+ * @param int $deep
+ * @return string
+ */
+function get_filterend($deep = -1) {
+  $result = '';
+  if (-1 === $deep) return $result;
+  $deeptab = "\t\t\t";
+  for ($i = 0; $i < $deep; ++$i) {
+    $deeptab .= "\t";
+  }
+  $result .= $deeptab."</Filter>\r\n";
+  return $result;
 }
 
 /**
@@ -100,44 +135,54 @@ function get_vcfiles($type, $files = array(), $dirpath = NULL) {
       $file = str_replace($dirpath, '', $file);
     $dirname = str_replace($filename, '', $file);
     $pathdeep = substr_count($dirname, $delimiter);
-    if (0 == $pathdeep && !in_array($dirname, $basedir)) {
-      array_push($basedir, $dirname);
-    } else {
+    $pos = strpos($dirname, '\\');
+    $fatherdir = substr($dirname, 0, $pos);
+    if (!in_array($fatherdir, $basedir)) {
+      array_push($basedir, $fatherdir);
+    } 
+    if (!in_array($dirname, $childdir)) {
       array_push($childdir, $dirname);
     }
     if (!array_key_exists($dirname, $temp)) {
       $temp[$dirname] = array();
     }
-    $vcfile = get_vcfile($pathdeep, $isdir, '..'.$delimiter.$type.$dirname);
-    array_push($temp[$dirname], $vcfile);
+    $vcfile = 
+      get_vcfile($pathdeep, $isdir, '..'.DELIMITER.$type.DELIMITER.$file);
+    $temp[$dirname] = $vcfile;
   }
   $vcinfo = array();
+  //get last directorys
+  $lastdirectorys = array();
   foreach ($basedir as $firstdir) {
-    $vcinfo[$firstdir] = array();
+    $lastdirectorys[$firstdir] = NULL;
     foreach ($childdir as $otherdir) {
       if (indir($firstdir, $otherdir)) {
-        $childdeep = substr_count($otherdir, $delimiter);
-        $dirdeep = count($vcinfo[$firstdir]);
-        if (0 === $dirdeep) { //hash is not has
-          for ($i = 0; $i < $childdeep + 1; ++$i) {
-            array_push($vcinfo[$firstdir], ''); //first construct
-          }
-          $vcinfo[$firstdir][0] = get_vcfile(0, false, $firstdir);
-          $vcinfo[$firstdir][$childdeep] = 
-            get_vcfile($childdeep, false, $otherdir);
-        }
-        elseif ($childdeep > $dirdeep) {
-          $right = $childdeep - $dirdeep;
-          for ($i = 0; $right; ++$i) {
-            array_push($vcinfo[$firstdir], ''); //first construct
-          }
-        }
-        else {
-          
-        }
+        $childdeep = substr_count($otherdir, DELIMITER);
+        $dirdeep = substr_count($lastdirectorys[$firstdir], DELIMITER);
+        if ($childdeep > $dirdeep) $lastdirectorys[$firstdir] = $otherdir;
       }
     }
   }
+  var_dump($lastdirectorys);
+  //get Filters info
+  foreach ($lastdirectorys as $firstdir => $lastdirectory) {
+    $vcinfo[$firstdir] = '';
+    if (NULL === $lastdirectory) $lastdirectory = $firstdir;
+    $directorys = explode($delimiter, $lastdirectory);
+    foreach ($directorys as $deep => $directory) {
+      echo $directory.LF;
+      $vcinfo[$firstdir] .= get_vcfile($deep, true, $directory);
+      $finddir = getdirectory($directorys, $deep);
+      if (array_key_exists($finddir, $temp))
+        $vcinfo[$firstdir] .= $temp[$finddir];
+    }
+    $maxdeep = count($directorys);
+    for ($i = $maxdeep; $i >= 0; --$i) {
+      $vcinfo[$firstdir] .= get_filterend($i);
+    }
+  }
+  $vcstring = implode('', $vcinfo);
+  return $vcstring;
 }
 
 /**
@@ -149,9 +194,19 @@ function main() {
   $argc = $GLOBALS['argc'];
   $argv = $GLOBALS['argv'];
   if ($argc != 2) {
-    echo 'param error';
+    echo 'param error'.LF;
     return 1;
   }
   $projectpath = $argv[1];
+  $projectpath = complementpath($projectpath, DELIMITER);
+  $includepath = $projectpath.'include';
+  $srcpath = $projectpath.'src';
+  if (!is_dir($includepath) || !is_dir($srcpath)) {
+    echo 'project not a standard project',LF;
+    return 1;
+  }
+  $includefiles = traverse($includepath);
+  echo get_vcfiles('include', $includefiles, $includepath.DELIMITER);
   return 0;
 }
+main();
